473c91214c0850b61585baf062c9932ff2ef797c
Sandun Sampath Vitharana <vitharana11996@gmail.com>
2025-06-03 23:06:58 -0500

    add file

---
+++ test_1.py
@@ -0,0 +1,22 @@
+from TTS.api import TTS
+import sounddevice as sd
+import numpy as np
+
+# Initialize TTS with a pretrained model (this downloads if needed)
+tts = TTS(model_name="tts_models/en/ljspeech/glow-tts")
+
+# Synthesize speech to numpy array
+text_samples = ["hey how are you", "how old are you", "where do you live", "he is tall"]
+
+
+for sample in text_samples:
+
+
+    wav = tts.tts(sample)
+
+    # Get sample rate (usually 22050 or 24000 depending on model)
+    sr = tts.synthesizer.output_sample_rate
+
+    # Play audio directly from numpy array
+    sd.play(wav, sr)
+    sd.wait()
